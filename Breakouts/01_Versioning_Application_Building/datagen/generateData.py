e04ed3b30295be5023119035be29daa463ea1261
Isaac Shivvers <ishivvers@berkeley.edu>
2012-09-08 15:15:30 -0700

    Final draft version of lecture slides (week 2) added, as well has homework assignment, and breakout files, and class example files.

---
+++ Breakouts/01_Versioning_Application_Building/datagen/generateData.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pylab
+'''This is a fairly useless program which uses pylab.rand() to generate some 
+data.  
+
+Example Usage:
+num_to_generate = 500
+data = pylab.rand(num_to_generate)
+
+"""
+'''
+def randData(num_to_generate):
+    mydata = pylab.rand(num_to_generate)
+    return mydata
+
