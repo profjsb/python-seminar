6b4ef121a71d64aa42ecedf28cbe8a1695044303
Joshua Bloom <profjsb@gmail.com>
2012-10-01 12:24:25 -0700

    morse code

---
+++ DataFiles_and_Notebooks/04_Interacting_with_the_World/morse_code.py
@@ -0,0 +1,78 @@
+# Josh had to install http://prolificusa.com/pl-2303hx-drivers/
+#
+import serial
+import time
+
+def blink(s, morse_code):
+    if morse_code == " ":
+        time.sleep(1.0)
+    if morse_code == ".":
+        s.setDTR(True) # Turns light on
+        time.sleep(0.25)
+        s.setDTR(False) # Turns light off
+    if morse_code == "-":
+        s.setDTR(True) # Turns light on
+        time.sleep(0.50)
+        s.setDTR(False) # Turns light off
+
+morsetab = {
+        'A': '.-',              'a': '.-',
+        'B': '-...',            'b': '-...',
+        'C': '-.-.',            'c': '-.-.',
+        'D': '-..',             'd': '-..',
+        'E': '.',               'e': '.',
+        'F': '..-.',            'f': '..-.',
+        'G': '--.',             'g': '--.',
+        'H': '....',            'h': '....',
+        'I': '..',              'i': '..',
+        'J': '.---',            'j': '.---',
+        'K': '-.-',             'k': '-.-',
+        'L': '.-..',            'l': '.-..',
+        'M': '--',              'm': '--',
+        'N': '-.',              'n': '-.',
+        'O': '---',             'o': '---',
+        'P': '.--.',            'p': '.--.',
+        'Q': '--.-',            'q': '--.-',
+        'R': '.-.',             'r': '.-.',
+        'S': '...',             's': '...',
+        'T': '-',               't': '-',
+        'U': '..-',             'u': '..-',
+        'V': '...-',            'v': '...-',
+        'W': '.--',             'w': '.--',
+        'X': '-..-',            'x': '-..-',
+        'Y': '-.--',            'y': '-.--',
+        'Z': '--..',            'z': '--..',
+        '0': '-----',           ',': '--..--',
+        '1': '.----',           '.': '.-.-.-',
+        '2': '..---',           '?': '..--..',
+        '3': '...--',           ';': '-.-.-.',
+        '4': '....-',           ':': '---...',
+        '5': '.....',           "'": '.----.',
+        '6': '-....',           '-': '-....-',
+        '7': '--...',           '/': '-..-.',
+        '8': '---..',           '(': '-.--.-',
+        '9': '----.',           ')': '-.--.-',
+        ' ': ' ',               '_': '..--.-',
+}
+
+print "Please enter message to transmit."
+msg = raw_input()
+byte_length = len(msg)
+s = serial.Serial('/dev/tty.usbserial')
+start_time = time.time()
+for c in msg:
+    m_code = "!"
+    try:
+        m_code = morsetab[c]
+    except KeyError:
+        print c, "has no morse code translation, skipping."
+    if m_code != "!":
+        print c, m_code
+        for elem in m_code:
+            blink(s, elem)
+            time.sleep(0.1)
+end_time = time.time()
+s.close()
+transmission_time = end_time - start_time
+print "Message transmission time:", round(transmission_time, 2), "seconds."
+print "Data rate:", round(byte_length/transmission_time, 2), "bytes/sec."
