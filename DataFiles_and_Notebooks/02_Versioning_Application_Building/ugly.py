f144e55a3f8540fcdf05820ca0c68b40619beab7
jbloom <profjsb@gmail.com>
2022-02-04 12:25:21 -0800

    ugly.py example of bad PEP8

---
+++ DataFiles_and_Notebooks/02_Versioning_Application_Building/ugly.py
@@ -0,0 +1,8 @@
+import pandas
+import os
+"""love those DOCSTRINGs"""
+# not a great snippet of code here
+if not results.fill:
+            histOutline.OutlinedHistogram(data,title   = title)
+else:
+            histOutline.RegularHistogram(data,title  =   title)
